1407d953b56abdcab10331e9e51c5070dfa3a0d0
mohemohe <mohemohe@ghippos.net>
2020-01-25 20:06:53 +0900

    多分動くんじゃないかなぁ

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # script-docker
+
+[![Build Status](https://cloud.drone.io/api/badges/lxbot/script-docker/status.svg)](https://cloud.drone.io/lxbot/script-docker)
+
 run in docker
--- script.go
+++ script.go
@@ -1,24 +1,40 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"encoding/gob"
+	"github.com/lxbot/script-docker/buff"
+	"log"
 	"os"
+	"os/exec"
 	"plugin"
 	"strings"
+	"sync"
+	"time"
 )
 
 type M = map[string]interface{}
+
+const lines = 30
 
 var store *plugin.Plugin
 var ch *chan M
 
 var images = map[string]string{
-	"node": "node:lts-alpine",
-	"ruby": "ruby:alpine",
+	"bash":   "archlinux/base",
+	"node":   "node:lts-alpine",
+	"php":    "php:alpine",
+	"python": "python:alpine",
+	"ruby":   "ruby:alpine",
 }
 
 func Boot(s *plugin.Plugin, c *chan M) {
 	store = s
 	ch = c
+
+	gob.Register(M{})
+	gob.Register([]interface{}{})
 }
 
 func OnMessage() []func(M) M {
@@ -30,11 +46,11 @@ func OnMessage() []func(M) M {
 				return nil
 			}
 
+			log.Println("[docker]", "use:", img)
 			script := extractScript(text)
-			exec(msg, img, script)
-
-			msg["mode"] = "reply"
-			return msg
+			log.Println("[docker]", "script:", script)
+			run(msg, img, script)
+			return nil
 		},
 	}
 }
@@ -65,6 +81,138 @@ func extractScript(text string) string {
 	return strings.Join(strings.Split(text, "\n")[1:], "\n")
 }
 
-func exec(msg M, img string, script string) {
+func deepCopy(msg M) (M, error) {
+	var b bytes.Buffer
+	e := gob.NewEncoder(&b)
+	d := gob.NewDecoder(&b)
+	if err := e.Encode(msg); err != nil {
+		return nil, err
+	}
+	r := map[string]interface{}{}
+	if err := d.Decode(&r); err != nil {
+		return nil, err
+	}
+	return r, nil
+}
 
+func generateText(stdout []string, stderr []string, tag string) string {
+	stdoutText := strings.Join(stdout, "\n")
+	stderrText := strings.Join(stderr, "\n")
+
+	return "STDOUT " + tag + "\n\n" + "```\n" + stdoutText + "\n```\n\nSTDERR " + tag + "\n\n" + "```\n" + stderrText + "\n```"
+}
+
+func run(msg M, img string, script string) {
+	args := []string{"run", "--rm", "-i", "--log-driver", "none"}
+	network := os.Getenv("LXBOT_ALLOW_DOCKER_NETWORK")
+	if network != "true" {
+		args = append(args, "--network", "none")
+	}
+	args = append(args, img)
+
+	cmd := exec.Command("docker", args...)
+	stdin, _ := cmd.StdinPipe()
+	stdout, _ := cmd.StdoutPipe()
+	stderr, _ := cmd.StderrPipe()
+
+	cmd.Start()
+
+	stdoutBuff := buff.NewBuff()
+	stderrBuff := buff.NewBuff()
+	buffCh := make(chan int)
+	timeout := false
+
+	wg := &sync.WaitGroup{}
+	go func() {
+		wg.Add(1)
+
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			stdoutBuff.Enqueue(scanner.Text())
+			buffCh <- 1
+		}
+
+		wg.Done()
+	}()
+	go func() {
+		wg.Add(1)
+
+		scanner := bufio.NewScanner(stderr)
+		for scanner.Scan() {
+			stderrBuff.Enqueue(scanner.Text())
+			buffCh <- 1
+		}
+
+		wg.Done()
+	}()
+	go func() {
+		waitCh := time.After(3 * time.Second)
+
+		for {
+			select {
+			case _, ok := <-buffCh:
+				if !ok {
+					return
+				}
+				if stdoutBuff.Len() >= lines || stderrBuff.Len() >= lines {
+					waitCh = time.After(3 * time.Second)
+
+					stdoutLines := stdoutBuff.BulkDequeue(lines)
+					stderrLines := stderrBuff.BulkDequeue(lines)
+					text := generateText(stdoutLines, stderrLines, "(PARTIAL)")
+
+					// FIXME: copy error
+					nextMsg, _ := deepCopy(msg)
+					nextMsg["mode"] = "reply"
+					msg["message"].(M)["text"] = text
+					*ch <- msg
+				}
+				break
+			case <-waitCh:
+				waitCh = time.After(3 * time.Second)
+
+				stdoutLines := stdoutBuff.DequeueALL()
+				stderrLines := stderrBuff.DequeueALL()
+				text := generateText(stdoutLines, stderrLines, "(PARTIAL)")
+
+				// FIXME: copy error
+				nextMsg, _ := deepCopy(msg)
+				nextMsg["mode"] = "reply"
+				msg["message"].(M)["text"] = text
+				*ch <- msg
+				break
+			case <-time.After(3 * time.Minute):
+				if !cmd.ProcessState.Exited() {
+					_ = cmd.Process.Kill()
+				}
+				timeout = true
+				return
+			}
+		}
+	}()
+
+	_, _ = stdin.Write([]byte(script))
+	_ = stdin.Close()
+
+	wg.Wait()
+	cmd.Wait()
+	close(buffCh)
+
+	tag := "(FINISH)"
+	if timeout {
+		tag = "(TIMEOUT)"
+	}
+	stdoutLines := stdoutBuff.DequeueALL()
+	stderrLines := stderrBuff.DequeueALL()
+	text := generateText(stdoutLines, stderrLines, tag)
+
+	// FIXME: copy error
+	nextMsg, err := deepCopy(msg)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	nextMsg["mode"] = "reply"
+	msg["message"].(M)["text"] = text
+	*ch <- msg
 }
